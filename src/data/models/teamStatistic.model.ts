7754cd23cd90eb57b2377395f95d0edbfe9cd6b2
Bianca Barbosa <yume_shiro@hotmail.com>
2023-05-26 02:33:41 -0300

    feat: adding lineup in team statistic model

--- src/data/models/teamStatistic.model.ts
+++ src/data/models/teamStatistic.model.ts
@@ -3,21 +3,28 @@ import { GoalsModel } from "./goals.model";
 import { LeagueModel } from "./league.model";
 import { TeamModel } from "./team.model";
 
+interface ILineup {
+  formation: string;
+  played: number;
+}
 export class TeamStatisticModel {
   league: LeagueModel;
   team: TeamModel;
   fixtures: FixturesModel;
   goals: GoalsModel;
+  lineups: Array<ILineup>;
 
   constructor(
     league: LeagueModel,
     team: TeamModel,
     fixtures: FixturesModel,
-    goals: GoalsModel
+    goals: GoalsModel,
+    lineups: Array<ILineup>
   ) {
     this.league = league;
     this.team = team;
     this.fixtures = fixtures;
     this.goals = goals;
+    this.lineups = lineups;
   }
 }
