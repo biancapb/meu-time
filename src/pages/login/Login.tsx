101fcf32e9a2f719abbd9365cebbbeee1a4a77a5
Bianca Barbosa <yume_shiro@hotmail.com>
2023-05-27 00:06:34 -0300

    feat: adding material ui and implementing textfield

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -2,6 +2,7 @@ import { useContext, useState } from "react";
 import UserContext from "../../data/contexts/user";
 import { validateAuthenticationKey } from "../../data/services/validate-authentication-key";
 import Button from "../../ui/components/Button/Button";
+import TextField from "@mui/material/TextField";
 
 const Login = () => {
   const [keyAuth, setKeyAuth] = useState<string>("");
@@ -30,8 +31,21 @@ const Login = () => {
 
   return (
     <div>
-      Login
-      <input type="text" onChange={(e) => handleChange(e)}></input>
+      <TextField
+        required
+        id="outlined-required"
+        label="API Key"
+        placeholder="Insira sua API Key"
+        onChange={handleChange}
+        inputProps={{
+          style: {
+            backgroundColor: "#D1E7FD",
+            display: "flex",
+            width: "340px",
+            borderRadius: "10px",
+          },
+        }}
+      />
       <Button
         onClick={() => handleClick()}
         textButton="Entrar"
