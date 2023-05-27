8279fc55da6e77d9f84c6d674c5aff4f0977d926
Bianca Barbosa <yume_shiro@hotmail.com>
2023-05-27 02:07:22 -0300

    feat: implementing logo icon in login layout

--- src/ui/components/LayoutLogin/LayoutLogin.tsx
+++ src/ui/components/LayoutLogin/LayoutLogin.tsx
@@ -4,7 +4,14 @@ import { validateAuthenticationKey } from "../../../data/services/validate-authe
 import ImageLayout from "../ImageLogin/ImageLogin";
 import { useContext, useState } from "react";
 import Button from "../Button/Button";
-import { Container, ContainerForm } from "./LayoutLoginStyle";
+import {
+  Container,
+  FormContainer,
+  LoginContainer,
+  LogoContainer,
+  LogoIcon,
+  Title,
+} from "./LayoutLoginStyle";
 
 const LayoutLogin: React.FC = () => {
   const [keyAuth, setKeyAuth] = useState<string>("");
@@ -34,28 +41,34 @@ const LayoutLogin: React.FC = () => {
   return (
     <Container>
       <ImageLayout />
-      <ContainerForm>
-        <TextField
-          required
-          id="outlined-required"
-          label="API Key"
-          placeholder="Insira sua API Key"
-          onChange={handleChange}
-          inputProps={{
-            style: {
-              backgroundColor: "#D1E7FD",
-              display: "flex",
-              width: "310px",
-              borderRadius: "10px",
-            },
-          }}
-        />
-        <Button
-          onClick={() => handleClick()}
-          textButton="Entrar"
-          route={isValidKey ? "/home" : "/"}
-        />
-      </ContainerForm>
+      <LoginContainer>
+        <LogoContainer>
+          <LogoIcon src="/assets/icons/meu-time-logo.svg" />
+          <Title>Meu Time</Title>
+        </LogoContainer>
+        <FormContainer>
+          <TextField
+            required
+            id="outlined-required"
+            label="API Key"
+            placeholder="Insira sua API Key"
+            onChange={handleChange}
+            inputProps={{
+              style: {
+                backgroundColor: "#D1E7FD",
+                display: "flex",
+                width: "310px",
+                borderRadius: "10px",
+              },
+            }}
+          />
+          <Button
+            onClick={() => handleClick()}
+            textButton="Entrar"
+            route={isValidKey ? "/home" : "/"}
+          />
+        </FormContainer>
+      </LoginContainer>
     </Container>
   );
 };
--- src/ui/components/LayoutLogin/LayoutLoginStyle.ts
+++ src/ui/components/LayoutLogin/LayoutLoginStyle.ts
@@ -1,28 +1,43 @@
 import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
-  align-items: center;
-  flex-wrap: wrap;
-  align-content: center;
   height: 100vh;
 `;
 
-export const ContainerForm = styled.div`
+export const LoginContainer = styled.div`
   display: flex;
-  height: 100%;
-  width: 50vw;
-
-  gap: 40px;
-
   flex-direction: column;
-
   place-items: center;
   place-content: center;
+  height: 100%;
+  width: 50vw;
+  gap: 120px;
 
   @media (max-width: 900px) {
     width: 100vw;
     justify-content: center;
   }
+`;
+
+export const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  place-items: center;
+`;
+
+export const LogoIcon = styled.img`
+  height: 100px;
+  width: 100px;
+`;
+
+export const Title = styled.h1`
+  font-size: 24px;
+`;
+
+export const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
 `;
 
 export const LoginImageContainer = styled.img`
