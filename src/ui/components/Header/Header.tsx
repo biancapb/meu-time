6e19a3bb1e8eb789f023b37d7fed32424b6d0fdc
Bianca Barbosa <yume_shiro@hotmail.com>
2023-05-27 03:19:41 -0300

    feat: creating header component

--- src/ui/components/Header/Header.tsx
+++ src/ui/components/Header/Header.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { Container, LogoIcon, LogoutIcon } from "./HeaderStyle";
 
 const Header: React.FC = () => {
-  return <>Header</>;
+  return (
+    <Container>
+      <LogoIcon src="/assets/icons/meu-time-logo.svg" />
+      <LogoutIcon src="/assets/icons/logout.svg" />
+    </Container>
+  );
 };
 export default Header;
--- src/ui/components/Header/HeaderStyle.ts
+++ src/ui/components/Header/HeaderStyle.ts
@@ -1,1 +1,21 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 80px;
+  width: 100vw;
+  background-color: #6fb7fe;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
+`;
+
+export const LogoIcon = styled.img`
+  height: 50px;
+  margin-left: 40px;
+`;
+
+export const LogoutIcon = styled.img`
+  height: 25px;
+  margin-right: 40px;
+`;
