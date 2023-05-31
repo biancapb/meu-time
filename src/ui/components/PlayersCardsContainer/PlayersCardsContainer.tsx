0ccb176a1f896d7b52a5a4f15a96ce91fc48f248
Bianca Barbosa <yume_shiro@hotmail.com>
2023-05-31 00:09:15 -0300

    feat: creating playerscardscontainer component structure

---
+++ src/ui/components/PlayersCardsContainer/PlayersCardsContainer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import PlayerCard from "../PlayerCard/PlayerCard";
+type Props = {};
+
+const ContainerCard = (props: Props) => {
+  return (
+    <div>
+      <PlayerCard />
+    </div>
+  );
+};
+
+export default ContainerCard;
